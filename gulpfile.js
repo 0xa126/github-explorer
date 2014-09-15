d0e495e51f23ec44640adf18d0376b481271ed71
okulshitskaya <okulshitskaya@seek.com.au>
2014-09-15 12:15:51 +1000

    updated gulpfile.js to include tasks for jshint and less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var less = require('gulp-less');
 
-gulp.task('default', function() {
-	console.log('hello world');
+var paths = {
+  scripts: ['./app/app.js'],
+  styles: ['./app/app.less']
+};
+
+gulp.task('jshint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
+
+gulp.task('less', function() {
+  return gulp.src(paths.styles)
+    .pipe(less({
+      paths: []
+    }))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['jshint']);
+  gulp.watch(paths.styles, ['less']);
+});
+
+gulp.task('default', ['jshint', 'less', 'watch']);
