cfa042fa2763db841d8b754bf60201733f8262b9
okulshitskaya <okulshitskaya@seek.com.au>
2014-09-19 13:30:17 +1000

    added gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var less = require('gulp-less');
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    less = require('gulp-less'),
+    livereload = require('gulp-livereload');
 
 var paths = {
   scripts: ['./app/app.js'],
@@ -22,8 +23,10 @@ gulp.task('less', function() {
 });
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch(paths.scripts, ['jshint']);
-  gulp.watch(paths.styles, ['less']);
+  gulp.watch(paths.styles, ['less']);  
+  gulp.watch('app/**').on('change', livereload.changed);
 });
 
 gulp.task('default', ['jshint', 'less', 'watch']);
